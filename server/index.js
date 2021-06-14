f5e9604512697fa081069612d72b5ac6512db269
Mita57 <57thrasher57@gmail.com>
2021-06-15 01:59:51 +0500

    Hello

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const express = require('express');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+})
